7cd240f1e5852af6bc23fb2ce4f35374acc0d038
artemsheludko <sheludko.artem@gmail.com>
2017-04-25 16:37:40 +0300

    add gulp-cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    prefix = require('gulp-autoprefixer');
+    prefix = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano');
