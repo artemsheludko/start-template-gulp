04182b546dc1ce6d45c31b44070f4caf24b73052
artemsheludko <sheludko.artem@gmail.com>
2017-04-25 18:25:05 +0300

    add html,css,js

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,45 @@ var gulp = require('gulp'),
     cssnano = require('gulp-cssnano'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    notify = require('gulp-notify');
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'build'
+        },
+        notify: false
+    });
+});
+
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('build'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('sass', function() {
+    return gulp.src('src/css/scss/style.scss')
+        .pipe(sass({outputStyle: 'expanded'}).on("error", notify.onError()))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('js', function() {
+    return gulp.src('src/js/main.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+// Watch
+gulp.task('watch', ['html', 'sass', 'js', 'browser-sync'], function() {
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/css/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['js']);    
+});
+
+// Default task
+gulp.task('default', ['watch']);
